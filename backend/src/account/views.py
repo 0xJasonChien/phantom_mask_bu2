100f0aa8bb74e2a152f6e8b6e1a5ef2a1cc681c8
0xJasonChien <jason891588@gmail.com>
2025-06-25 20:35:38 +0800

    [fix] replace swagger_auto_schema with extend_schema

--- backend/src/account/views.py
+++ backend/src/account/views.py
@@ -4,14 +4,11 @@ from captcha.models import CaptchaStore
 from captcha.views import captcha_image
 from django.http import HttpRequest, HttpResponse
 from drf_spectacular.utils import extend_schema
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-from account.apps import AccountConfig
 
 from .models import User
 from .serializers import (
@@ -27,12 +24,11 @@ class UserCreateView(CreateAPIView):
     permission_classes = ()
     queryset = User.objects.all()
 
-    @swagger_auto_schema(
+    @extend_schema(
         operation_id='建立使用者帳號',
         responses={
             HTTP_201_CREATED: TokenRetrieveSerializer,
         },
-        tags=(AccountConfig.name,),
     )
     def post(
         self: Self,
